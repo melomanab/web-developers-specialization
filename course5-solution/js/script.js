a733b363992f6a762fe188a3a2ea71813265b0b2
bmoreno <beatriz.moreno@soprasteria.com>
2025-07-25 11:43:12 +0200

    Get the short name of a randomly selected category

--- course5-solution/js/script.js
+++ course5-solution/js/script.js
@@ -91,20 +91,22 @@ $ajaxUtils.sendGetRequest(
 });
 // *** finish **
 
-
+// =======================================
+// BUILD HOME PAGE WITH RANDOM CATEGORY
+// =======================================
 // Builds HTML for the home page based on categories array
 // returned from the server.
 function buildAndShowHomeHTML (categories) {
 
-  // Load home snippet page
   $ajaxUtils.sendGetRequest(
+  // Load home snippet page
     homeHtmlUrl,
     function (homeHtml) {
-
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      // var chosenCategoryShortName = ....
+      var chosenCategory = chooseRandomCategory(categories);
+      var chosenCategoryShortName = chosenCategory.short_name;
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
