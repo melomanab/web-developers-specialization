9156051cbce1160c2743717c5d340fca83911113
bmoreno <beatriz.moreno@soprasteria.com>
2025-07-25 11:44:51 +0200

    Build dynamically the home snippet using the random category short name

--- course5-solution/js/script.js
+++ course5-solution/js/script.js
@@ -110,7 +110,9 @@ function buildAndShowHomeHTML (categories) {
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
-      // chosen category from STEP 2. Use existing insertProperty function for that purpose.
+      // chosen category from STEP 2.
+
+      // Use existing insertProperty function for that purpose.
       // Look through this code for an example of how to do use the insertProperty function.
       // WARNING! You are inserting something that will have to result in a valid Javascript
       // syntax because the substitution of {{randomCategoryShortName}} becomes an argument
@@ -120,7 +122,11 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      // var homeHtmlToInsertIntoMainPage = ....
+      var homeHtmlToInsertIntoMainPage = insertProperty(
+        homeHtml,
+        "randomCategoryShortName",
+        "'" + chosenCategoryShortName + "'"
+      );
 
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
