86e0801712c00c31dd710f5d69b523c5f6fb4080
bmoreno <beatriz.moreno@soprasteria.com>
2025-07-25 11:41:19 +0200

    On fist loading, call the function that builds and returns the homepage

--- course5-solution/js/script.js
+++ course5-solution/js/script.js
@@ -78,12 +78,15 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // TODO: STEP 1: Substitute [...] below with the *value* of the function buildAndShowHomeHTML,
 // so it can be called when server responds with the categories data.
 
+// =======================================
+// FIRST LOAD
+// =======================================
 // *** start ***
 // On first load, show home view
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  [...], // ***** <---- TODO: STEP 1: Substitute [...] ******
+  buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
